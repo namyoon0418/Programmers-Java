f351d1bc37913e1209da56ee73f45d927c393222
namyoon0418 <namyoon0418@gmail.com>
2026-09-22 13:04:32 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int multiply = 1;
+        int sum = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            multiply *= num_list[i];
+            sum += num_list[i];
+        }
+        
+        if (multiply < sum * sum) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
